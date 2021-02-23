cb6001633c9f2c178b21af20b240aa15aaa4f3b0
Murilo Ramos <muri.ilo@hotmail.com>
2021-02-23 18:10:56 -0300

    Criação de Def's para professor

--- professor.rb
+++ professor.rb
@@ -1,10 +1,31 @@
 class Professor
-    attr_accessor :nome, :codigo_funcionario
+    attr_reader :ferias
+    attr_accessor :nome, :codigo_funcionario, :disciplina, :inicio_ferias, :fim_ferias
 
-    def initialize (nome, codigo_funcionario)
+    def initialize (nome, codigo_funcionario, disciplina)
 
         @nome = nome
         @codigo_funcionario = codigo_funcionario
+        @disciplina = disciplina
+        @ferias = false
+        @inicio_ferias = inicia_ferias()
+        @fim_ferias = encerra_ferias()
     end
-        
+
+    def inicia_ferias()
+        @ferias = true
+        @inicio_ferias = Time.now()
+        @fim_ferias = nil
+    end
+
+    def encerra_ferias()
+        @ferias = false
+        @fim_ferias = Time.now
+        @inicio_ferias =nil
+    end
+
+
+
+
+
 end
